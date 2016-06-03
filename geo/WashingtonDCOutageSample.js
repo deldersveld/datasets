fcfc46b8be55c224a2b51f25a9cd6f697490a598
David Eldersveld <deldersveld@blue-granite.com>
2016-06-03 09:41:00 -0400

    DC outage sample geography

---
+++ geo/WashingtonDCOutageSample.js
@@ -0,0 +1,325 @@
+var dcOutages = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.0469,38.91609 ]
+    },
+    "properties": {
+    "OutageID":1,
+    "OutageReported":"5/16/16 5:00",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 11:00",
+    "NumberOfCustomersAffected":325,
+    "Status":"Restored",
+    "CustomerBin":"251-1000 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.01484,38.89576 ]
+    },
+    "properties": {
+    "OutageID":2,
+    "OutageReported":"5/16/16 9:00",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 15:00",
+    "NumberOfCustomersAffected":400,
+    "Status":"On site",
+    "CustomerBin":"251-1000 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.99248,38.90978 ]
+    },
+    "properties": {
+    "OutageID":3,
+    "OutageReported":"5/16/16 9:15",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 15:15",
+    "NumberOfCustomersAffected":1700,
+    "Status":"Restored",
+    "CustomerBin":"1001-2500 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.00804,38.87564 ]
+    },
+    "properties": {
+    "OutageID":4,
+    "OutageReported":"5/16/16 9:20",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 15:20",
+    "NumberOfCustomersAffected":10,
+    "Status":"On site",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.97703,38.89273 ]
+    },
+    "properties": {
+    "OutageID":5,
+    "OutageReported":"5/16/16 10:00",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 16:00",
+    "NumberOfCustomersAffected":1,
+    "Status":"Restored",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.99847,38.91724 ]
+    },
+    "properties": {
+    "OutageID":6,
+    "OutageReported":"5/16/16 10:10",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 16:10",
+    "NumberOfCustomersAffected":525,
+    "Status":"On site",
+    "CustomerBin":"251-1000 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.01032,38.92757 ]
+    },
+    "properties": {
+    "OutageID":7,
+    "OutageReported":"5/16/16 10:30",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 16:30",
+    "NumberOfCustomersAffected":400,
+    "Status":"On site",
+    "CustomerBin":"251-1000 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.01471,38.90412 ]
+    },
+    "properties": {
+    "OutageID":8,
+    "OutageReported":"5/16/16 10:45",
+    "Cause":"Cause A",
+    "RestorationEstimate":"5/16/16 16:45",
+    "NumberOfCustomersAffected":50,
+    "Status":"Restored",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.00441,38.8966 ]
+    },
+    "properties": {
+    "OutageID":9,
+    "OutageReported":"5/16/16 11:00",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 17:00",
+    "NumberOfCustomersAffected":20,
+    "Status":"Restored",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.04426,38.8951 ]
+    },
+    "properties": {
+    "OutageID":10,
+    "OutageReported":"5/16/16 11:00",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 17:00",
+    "NumberOfCustomersAffected":475,
+    "Status":"Restored",
+    "CustomerBin":"251-1000 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.03533,38.92525 ]
+    },
+    "properties": {
+    "OutageID":11,
+    "OutageReported":"5/16/16 11:30",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 17:30",
+    "NumberOfCustomersAffected":30,
+    "Status":"Restored",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.02507,38.93223 ]
+    },
+    "properties": {
+    "OutageID":12,
+    "OutageReported":"5/16/16 12:00",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 18:00",
+    "NumberOfCustomersAffected":20,
+    "Status":"Restored",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.01247,38.94014 ]
+    },
+    "properties": {
+    "OutageID":13,
+    "OutageReported":"5/16/16 12:00",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 18:00",
+    "NumberOfCustomersAffected":10,
+    "Status":"On site",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.03423,38.94379 ]
+    },
+    "properties": {
+    "OutageID":14,
+    "OutageReported":"5/16/16 12:15",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 18:15",
+    "NumberOfCustomersAffected":125,
+    "Status":"On site",
+    "CustomerBin":"51-250 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -77.0199,38.97112 ]
+    },
+    "properties": {
+    "OutageID":15,
+    "OutageReported":"5/16/16 12:15",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 18:15",
+    "NumberOfCustomersAffected":900,
+    "Status":"On site",
+    "CustomerBin":"251-1000 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.99954,38.96089 ]
+    },
+    "properties": {
+    "OutageID":16,
+    "OutageReported":"5/16/16 12:45",
+    "Cause":"Cause B",
+    "RestorationEstimate":"5/16/16 18:45",
+    "NumberOfCustomersAffected":1300,
+    "Status":"On site",
+    "CustomerBin":"1001-2500 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.98825,38.95013 ]
+    },
+    "properties": {
+    "OutageID":17,
+    "OutageReported":"5/16/16 13:00",
+    "Cause":"Undetermined",
+    "RestorationEstimate":"5/16/16 19:00",
+    "NumberOfCustomersAffected":30,
+    "Status":"Pending",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.97847,38.92878 ]
+    },
+    "properties": {
+    "OutageID":18,
+    "OutageReported":"5/16/16 13:10",
+    "Cause":"Undetermined",
+    "RestorationEstimate":"5/16/16 19:10",
+    "NumberOfCustomersAffected":50,
+    "Status":"Pending",
+    "CustomerBin":"1-50 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.96158,38.919 ]
+    },
+    "properties": {
+    "OutageID":19,
+    "OutageReported":"5/16/16 13:45",
+    "Cause":"Undetermined",
+    "RestorationEstimate":"5/16/16 19:45",
+    "NumberOfCustomersAffected":75,
+    "Status":"Pending",
+    "CustomerBin":"51-250 Customers Out"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.97566,38.89848 ]
+    },
+    "properties": {
+    "OutageID":20,
+    "OutageReported":"5/16/16 14:00",
+    "Cause":"Undetermined",
+    "RestorationEstimate":"5/16/16 20:00",
+    "NumberOfCustomersAffected":225,
+    "Status":"Pending",
+    "CustomerBin":"51-250 Customers Out"
+    }
+  }
+]
+}
